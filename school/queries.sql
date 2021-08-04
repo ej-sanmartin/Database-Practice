7765eb8d9dc1db9a20d4d4f41b2c7a0f3ab2995a
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-08-04 05:52:44 -0400

    Update queries.sql

--- school/queries.sql
+++ school/queries.sql
@@ -22,3 +22,31 @@ SELECT "Course Title"
 FROM class_information
 GROUP BY "Course Title"
 ORDER BY AVG("Student Grade") LIMIT 1;
+
+/* Get all teachers for Tracy Centerson */
+SELECT teacher
+FROM class_information
+WHERE student_id = (SELECT id FROM student
+                    WHERE first_name = 'Tracy' AND last_name = 'Centerson');
+                    
+/* Get all of the students Daniella Amos teaches */
+SELECT student
+FROM class_information
+WHERE teacher_id = (SELECT id FROM teacher
+                    WHERE first_name = 'Daniella' AND last_name = 'Amos');
+                    
+/* Get the averages of each subject and translate subject codes */
+SELECT
+  "Course Subject" AS "Subject Code",
+  CASE
+    WHEN "Course Subject" = 'V' THEN 'Visual Arts'
+    WHEN "Course Subject" = 'H' THEN 'History'
+    WHEN "Course Subject" = 'L' THEN 'Languages'
+    WHEN "Course Subject" = 'E' THEN 'English'
+    WHEN "Course Subject" = 'M' THEN 'Maths'
+    WHEN "Course Subject" = 'S' THEN 'Sciences'
+  END AS "Subject",
+  CAST(AVG("Student Grade") AS DECIMAL(5, 2)) AS "Subject Average"
+FROM class_information
+GROUP BY "Course Subject"
+ORDER BY (AVG("Student Grade")) DESC;
